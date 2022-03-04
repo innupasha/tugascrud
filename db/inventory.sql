f32df82664039baa3c7ff91096404ee4922ae9e1
Benny maulana <die_zn@yahoo.co.id>
2022-03-04 14:41:17 +0700

    update

--- db/inventory.sql
+++ db/inventory.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 26, 2022 at 08:44 AM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.10
+-- Generation Time: Mar 04, 2022 at 08:19 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -40,7 +39,7 @@ CREATE TABLE `notes` (
 --
 
 INSERT INTO `notes` (`id`, `contents`, `admin`, `status`) VALUES
-(21, 'Disini bisa tulis notes', 'Stock', 'aktif');
+(22, 'Disini bisa tulis notes', 'Benny', 'aktif');
 
 -- --------------------------------------------------------
 
@@ -57,13 +56,6 @@ CREATE TABLE `sbrg_keluar` (
   `keterangan` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `sbrg_keluar`
---
-
-INSERT INTO `sbrg_keluar` (`id`, `idx`, `tgl`, `jumlah`, `penerima`, `keterangan`) VALUES
-(15, 244, '2020-08-29', 1000, 'Kasmina', 'Laku');
-
 -- --------------------------------------------------------
 
 --
@@ -77,13 +69,6 @@ CREATE TABLE `sbrg_masuk` (
   `jumlah` int(11) NOT NULL,
   `keterangan` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `sbrg_masuk`
---
-
-INSERT INTO `sbrg_masuk` (`id`, `idx`, `tgl`, `jumlah`, `keterangan`) VALUES
-(9, 244, '2020-08-07', 600, 'kk');
 
 -- --------------------------------------------------------
 
@@ -104,7 +89,8 @@ CREATE TABLE `slogin` (
 --
 
 INSERT INTO `slogin` (`id`, `username`, `password`, `nickname`, `role`) VALUES
-(7, 'guest', '084e0343a0486ff05530df6c705c8bb4', 'Stock', 'stock');
+(7, 'guest', '084e0343a0486ff05530df6c705c8bb4', 'Guest', 'stock'),
+(13, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'stock');
 
 -- --------------------------------------------------------
 
@@ -128,8 +114,7 @@ CREATE TABLE `sstock_brg` (
 --
 
 INSERT INTO `sstock_brg` (`idx`, `nama`, `jenis`, `merk`, `ukuran`, `stock`, `satuan`, `lokasi`) VALUES
-(243, 'Mata Bor', 'Flame', 'Garryson', '50', 2992, 'Buah', 'PT Willtec'),
-(244, 'Mata Bor', 'Ball Nosed Cone', 'Garryson', '17', 1000, 'Unit', 'PT Wiltec');
+(245, 'Keyboard', 'Wireless', 'Logitech', '15', 50, 'Buah', 'Lab Informatika UNBARA');
 
 --
 -- Indexes for dumped tables
@@ -173,31 +158,31 @@ ALTER TABLE `sstock_brg`
 -- AUTO_INCREMENT for table `notes`
 --
 ALTER TABLE `notes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `sbrg_keluar`
 --
 ALTER TABLE `sbrg_keluar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `sbrg_masuk`
 --
 ALTER TABLE `sbrg_masuk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `slogin`
 --
 ALTER TABLE `slogin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `sstock_brg`
 --
 ALTER TABLE `sstock_brg`
-  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=245;
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=246;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
